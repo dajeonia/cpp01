68613b897aae81fe5a2022065367d2cddecb235b
Daemyeong Jeon <dajeon@c6r1s8.42seoul.kr>
2024-01-03 10:11:54 +0900

    ex04: 0.1

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <cstring>
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	std::ifstream file("text");
-	char buf[5];
-
-	while (file)
+	if (argc != 4)
 	{
-		file.read(buf, 4);
-		buf[file.gcount()] = '\0';
-		std::cout << buf;
+		std::cerr << "mySed: invalid argument" << std::endl;
+		return (1);
+	}
+
+	std::ifstream is;
+	is.open(argv[1]);
+	if (!is.is_open())
+	{
+		std::cerr << "mySed: Cannot open the file" << std::endl;
+		return (1);
+	}
+
+	std::ofstream os(std::string(argv[1]) + ".replace");
+	char c;
+	while (true)
+	{
+		is.get(c);
+		if (is.eof())
+			break ;
+		if (c != argv[2][0])
+			os << c;
+		else
+		{
+			std::stringstream ss;
+			ss << c;
+			if (!is.eof())
+			{
+				for (int i=1; argv[2][i]; ++i)
+				{
+					is.get(c);
+					ss << c;
+					if (c != argv[2][i])
+						break ;
+				}
+			}
+			if (ss.str() == std::string(argv[2]))
+				os << argv[3];
+			else
+				os << ss.str();
+		}
 	}
 	return (0);
 }
